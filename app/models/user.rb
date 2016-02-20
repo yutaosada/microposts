6fce8dd6497a14506b60394966786570f91930ee
yutaosada <yuta.19930803@gmail.com>
2016-02-20 08:47:45 +0000

    microposts4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :age , numericality: {greater_than_or_equal_to:0,only_integer:true} , presence: true, on: :update
+  validates :age , numericality: {greater_than_or_equal_to:0,less_than_or_equal_to:100,only_integer:true} , presence: true, on: :update
   
 end
